81d05582247e744fa0a2e94094d9c7d9f77e29d7
Lucio Solórzano <lucio.solorzano@gmail.com>
2022-10-26 21:53:39 -0500

    sdds

--- iOMG/forpagos.cs
+++ iOMG/forpagos.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace iOMG
 {
@@ -9,6 +11,19 @@ namespace iOMG
         DataTable dt;           // medios de pago
         string _mpefec = "";    // variable tipo de pago efectivo
         bool SoloLee = false;
+        double vlimban = 0;     // valor limite para bancarizar pagos en efectivo
+        string vglosa1 = "";    // glosa del limite bancariz
+        // string de conexion
+        static string serv = ConfigurationManager.AppSettings["serv"].ToString();
+        static string port = ConfigurationManager.AppSettings["port"].ToString();
+        static string usua = ConfigurationManager.AppSettings["user"].ToString();
+        static string cont = ConfigurationManager.AppSettings["pass"].ToString();
+        static string data = ConfigurationManager.AppSettings["data"].ToString();
+        static string ctl = ConfigurationManager.AppSettings["ConnectionLifeTime"].ToString();
+
+        string DB_CONN_STR = "server=" + serv + ";uid=" + usua + ";pwd=" + cont + ";database=" + data + " " + ";default command timeout=120" +
+            ";ConnectionLifeTime=" + ctl + ";";
+
         public forpagos(DataTable dtmp, string mpefec, string[,] idavuelta, bool sololee)
         {
             dt = dtmp;
@@ -31,6 +46,7 @@ namespace iOMG
         }
         private void forpagos_Load(object sender, EventArgs e)
         {
+            jalainfo();
             foreach (DataRow row in dt.Rows)
             {
                 cmb_plazo.Items.Add(row.ItemArray[0].ToString());
@@ -50,7 +66,39 @@ namespace iOMG
                 SendKeys.Send("{TAB}");
             }
         }
+        private void jalainfo()                                                 // obtiene datos de imagenes
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(DB_CONN_STR);
+                conn.Open();
+                string consulta = "select formulario,campo,param,valor from enlaces where formulario in(@nofo)";
+                MySqlCommand micon = new MySqlCommand(consulta, conn);
+                micon.Parameters.AddWithValue("@nofo", "forpagos");
+                MySqlDataAdapter da = new MySqlDataAdapter(micon);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                for (int t = 0; t < dt.Rows.Count; t++)
+                {
+                    DataRow row = dt.Rows[t];
+                    if (row["formulario"].ToString() == "forpagos")
+                    {
+                        if (row["campo"].ToString() == "efec_bancariz" && row["param"].ToString() == "limite") vlimban = double.Parse(row["valor"].ToString().Trim()); // valor limite para banzarizar efectivos
+                        if (row["campo"].ToString() == "efec_bancariz" && row["param"].ToString() == "glosa1") vglosa1 = row["valor"].ToString().Trim();               // glosa del limite bancarizacion
+                    }
+                }
+                da.Dispose();
+                dt.Dispose();
+                conn.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error de conexión");
+                Application.Exit();
 
+                return;
+            }
+        }
         public string ReturnValue1 { get; set; }
         public string[,] ReturnValue = new string[10, 7];
 
@@ -103,6 +151,16 @@ namespace iOMG
                 tx_importe.Focus();
                 return;
             }
+            if (tx_dat_mp.Text == _mpefec && vi >= vlimban)
+            {
+                var zz = MessageBox.Show(vglosa1 + Environment.NewLine + Environment.NewLine + 
+                    "Confirma que desea continuar?","Atención",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (zz == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
             dataGridView1.Rows.Add(0,dataGridView1.Rows.Count-1,cmb_plazo.Text,tx_numOpe.Text,tx_importe.Text,tx_dat_mp.Text,tx_fpago.Text);
             totalizagrid();
             //
